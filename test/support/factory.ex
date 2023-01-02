17d9941420559270121e3d57ebbaedadb1d7868c
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-01-02 19:12:41 -0300

    Test - Created factory for insert user data in db

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,5 +1,7 @@
 defmodule Rockelivery.Factory do
-  use ExMachina
+  use ExMachina.Ecto, repo: Rockelivery.Repo
+
+  alias Rockelivery.User
 
   def user_params_factory do
     %{
@@ -10,6 +12,19 @@ defmodule Rockelivery.Factory do
       email: "email@email",
       password: "123123",
       name: "Stark"
+    }
+  end
+
+  def user_factory do
+    %User{
+      age: 26,
+      address: "Winterfell",
+      cep: "00000000",
+      cpf: "00000000000",
+      email: "email@email",
+      password: "123123",
+      name: "Stark",
+      id: "957da868-ce7f-4eec-bcdc-97b8c992a60d"
     }
   end
 end
