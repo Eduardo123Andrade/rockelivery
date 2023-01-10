2a5b46963a865370fc67bb17983b367f6acfb732
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-01-10 13:16:52 -0300

    Test - Created item factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,7 +1,7 @@
 defmodule Rockelivery.Factory do
   use ExMachina.Ecto, repo: Rockelivery.Repo
 
-  alias Rockelivery.User
+  alias Rockelivery.{Item, User}
 
   def user_params_factory do
     %{
@@ -25,6 +25,25 @@ defmodule Rockelivery.Factory do
       password: "123123",
       name: "Stark",
       id: "957da868-ce7f-4eec-bcdc-97b8c992a60d"
+    }
+  end
+
+  def item_params_factory do
+    %{
+      category: :food,
+      description: "Pizza de frango",
+      photo: "https://www.google.com",
+      price: Decimal.new("30.0")
+    }
+  end
+
+  def item_factory do
+    %Item{
+      category: :food,
+      description: "Pizza de frango",
+      photo: "https://www.google.com",
+      price: Decimal.new("30.0"),
+      id: "70247b75-fd37-4c21-bead-ae4996696f9e"
     }
   end
 end
