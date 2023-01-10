29f2b45de4e520b71321ef13d8d256f098ba7e05
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-01-10 18:43:23 -0300

    Feat - create some other defdelegates for item

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,5 +1,8 @@
 defmodule Rockelivery do
   alias Rockelivery.Items.Create, as: ItemCreate
+  alias Rockelivery.Items.Get, as: ItemGet
+  alias Rockelivery.Items.Delete, as: ItemDelete
+  alias Rockelivery.Items.Update, as: ItemUpdate
 
   alias Rockelivery.Users.Create, as: UserCreate
   alias Rockelivery.Users.Delete, as: UserDelete
@@ -12,4 +15,7 @@ defmodule Rockelivery do
   defdelegate update_user(params), to: UserUpdate, as: :call
 
   defdelegate create_item(params), to: ItemCreate, as: :call
+  defdelegate get_item(id), to: ItemGet, as: :call
+  defdelegate delete_item(id), to: ItemDelete, as: :call
+  defdelegate update_item(params), to: ItemUpdate, as: :call
 end
