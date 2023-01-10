5fc48438106a002481287af48e24488755f7e37e
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-01-10 12:09:35 -0300

    Fix - Fixed error response on update user

--- lib/rockelivery/users/update.ex
+++ lib/rockelivery/users/update.ex
@@ -1,5 +1,7 @@
 defmodule Rockelivery.Users.Update do
   alias Rockelivery.{Error, Repo, User}
+  # alias Rockelivery.
+  # {Error, Repo, User}
 
   def call(%{"id" => id} = params) do
     case Repo.get(User, id) do
@@ -12,5 +14,12 @@ defmodule Rockelivery.Users.Update do
     user
     |> User.changeset(params)
     |> Repo.update()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %User{}} = result), do: result
+
+  defp handle_insert({:error, result}) do
+    {:error, Error.build(:bad_request, result)}
   end
 end
