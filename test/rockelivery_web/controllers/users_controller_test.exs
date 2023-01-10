d5ed63b92678fba17153e7b9365969638a74d56b
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-01-10 12:12:41 -0300

    Test - Created test for user update controller

--- test/rockelivery_web/controllers/users_controller_test.exs
+++ test/rockelivery_web/controllers/users_controller_test.exs
@@ -148,4 +148,73 @@ defmodule RockeliveryWeb.UsersControllerTest do
       assert %{"message" => "Invalid UUID"} = response
     end
   end
+
+  describe "update2" do
+    test "when all parameters are valid, return a updated user", %{conn: conn} do
+      id = "957da868-ce7f-4eec-bcdc-97b8c992a60d"
+      update_params = build(:user_params, %{name: "Lord Stark", age: 30, id: id})
+
+      insert(:user)
+
+      response =
+        conn
+        |> put(Routes.users_path(conn, :update, id), update_params)
+        |> json_response(:ok)
+
+      expected_response = %{
+        "user" => %{
+          "address" => "Winterfell",
+          "age" => 30,
+          "cpf" => "00000000000",
+          "email" => "email@email",
+          "id" => "957da868-ce7f-4eec-bcdc-97b8c992a60d",
+          "name" => "Lord Stark"
+        }
+      }
+
+      assert expected_response == response
+    end
+
+    test "when some params are invalid, return a error", %{conn: conn} do
+      id = "957da868-ce7f-4eec-bcdc-97b8c992a60d"
+      update_params = build(:user_params, %{age: 10, id: id})
+
+      insert(:user)
+
+      response =
+        conn
+        |> put(Routes.users_path(conn, :update, id), update_params)
+        |> json_response(:bad_request)
+
+      expected_response = %{"message" => %{"age" => ["must be greater than or equal to 18"]}}
+
+      assert expected_response == response
+    end
+
+    test "where user not found, return a error", %{conn: conn} do
+      id = "957da868-ce7f-4eec-bcdc-97b8c992a60a"
+
+      insert(:user)
+
+      response =
+        conn
+        |> delete(Routes.users_path(conn, :delete, id))
+        |> json_response(:not_found)
+
+      assert %{"message" => "User not found"} = response
+    end
+
+    test "where given a invalid id, return a error", %{conn: conn} do
+      id = "957da868-ce7f-4eec-bcdc-97b8c992a60"
+
+      insert(:user)
+
+      response =
+        conn
+        |> delete(Routes.users_path(conn, :delete, id))
+        |> json_response(:bad_request)
+
+      assert %{"message" => "Invalid UUID"} = response
+    end
+  end
 end
