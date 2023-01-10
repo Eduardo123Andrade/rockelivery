230e0154ab55bb217f90efd51969316e2f69c2c4
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-01-10 18:56:36 -0300

    Test - Created show item controller test

--- test/rockelivery_web/controllers/item_controller_test.exs
+++ test/rockelivery_web/controllers/item_controller_test.exs
@@ -51,4 +51,53 @@ defmodule RockeliveryWeb.ItemControllerTest do
       assert expected_response == response
     end
   end
+
+  describe "show/2" do
+    test "when find item with this id, return a item", %{conn: conn} do
+      %{id: id} = insert(:item)
+
+      response =
+        conn
+        |> get(Routes.items_path(conn, :show, id))
+        |> json_response(:ok)
+
+      expected_response = %{
+        "item" => %{
+          "category" => "food",
+          "description" => "Pizza de frango",
+          "id" => "70247b75-fd37-4c21-bead-ae4996696f9e",
+          "photo" => "https://www.google.com",
+          "price" => "30.0"
+        }
+      }
+
+      assert expected_response == response
+    end
+
+    test "when item not found, return a error", %{conn: conn} do
+      id = "957da868-ce7f-4eec-bcdc-97b8c992a60a"
+
+      insert(:item)
+
+      response =
+        conn
+        |> get(Routes.items_path(conn, :show, id))
+        |> json_response(:not_found)
+
+      assert %{"message" => "Item not found"} = response
+    end
+
+    test "where given a invalid id, return a error", %{conn: conn} do
+      id = "957da868-ce7f-4eec-bcdc-97b8c992a60"
+
+      insert(:item)
+
+      response =
+        conn
+        |> get(Routes.items_path(conn, :show, id))
+        |> json_response(:bad_request)
+
+      assert %{"message" => "Invalid UUID"} = response
+    end
+  end
 end
