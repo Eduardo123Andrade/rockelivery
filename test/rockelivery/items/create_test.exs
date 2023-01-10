fcef9ee2b8dd726796f9c4ed6884c3493a2d94a6
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-01-10 16:35:11 -0300

    Test - Created create item test

---
+++ test/rockelivery/items/create_test.exs
@@ -0,0 +1,31 @@
+defmodule Rockelivery.Items.CreateTest do
+  use Rockelivery.DataCase, async: true
+
+  import Rockelivery.Factory
+
+  alias Rockelivery.{Error, Item}
+  alias Rockelivery.Items.Create
+
+  describe "call/1" do
+    test "when all params are valid, return a item" do
+      response =
+        build(:item_params)
+        |> Create.call()
+
+      assert {:ok, %Item{category: :food, description: "Pizza de frango"}} = response
+    end
+
+    test "When there are invalid params, return a error" do
+      response =
+        build(:item_params, %{price: Decimal.new("0")})
+        |> Create.call()
+
+      expected_response = %{
+        price: ["must be greater than 0"]
+      }
+
+      assert {:error, %Error{result: changeset, status: :bad_request}} = response
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end
