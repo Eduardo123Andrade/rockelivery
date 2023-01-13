422be014041b48ba936533f039966f6ea34c7444
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-01-13 16:44:33 -0300

    Feat - Created test for via_cep client

--- lib/via_cep/client.ex
+++ lib/via_cep/client.ex
@@ -4,11 +4,11 @@ defmodule Rockelivery.ViaCep.Client do
   alias Rockelivery.Error
   alias Tesla.Env
 
-  plug Tesla.Middleware.BaseUrl, "https://viacep.com.br/ws/"
+  @base_url "https://viacep.com.br/ws/"
   plug Tesla.Middleware.JSON
 
-  def get_cep_info(cep) do
-    "#{cep}/json"
+  def get_cep_info(url \\ @base_url, cep) do
+    "#{url}#{cep}/json/"
     |> get()
     |> handle_get()
   end
@@ -20,7 +20,7 @@ defmodule Rockelivery.ViaCep.Client do
   defp handle_get({:ok, %Env{status: 200, body: body}}), do: {:ok, body}
 
   defp handle_get({:ok, %Env{body: _body, status: 400}}) do
-    {:error, Error.build(:not_found, "Invalid cep")}
+    {:error, Error.build(:bad_request, "Invalid cep")}
   end
 
   defp handle_get({:error, reason}) do
---
+++ test/rockelivery/via_cep/client_test.exs
@@ -0,0 +1,111 @@
+defmodule Rockelivery.ViaCep.ClientTest do
+  alias Plug.Conn
+  alias Rockelivery.ViaCep.Client
+  alias Rockelivery.Error
+
+  use ExUnit.Case, async: true
+
+  describe "get_cep_info/1" do
+    setup do
+      bypass = Bypass.open()
+
+      {:ok, bypass: bypass}
+    end
+
+    test "when there is a valid cep, return the cep info", %{bypass: bypass} do
+      cep = "01001000"
+
+      url = endpoint_url(bypass.port)
+
+      body = ~s({
+        "cep": "01001-000",
+        "logradouro": "Praça da Sé",
+        "complemento": "lado ímpar",
+        "bairro": "Sé",
+        "localidade": "São Paulo",
+        "uf": "SP",
+        "ibge": "3550308",
+        "gia": "1004",
+        "ddd": "11",
+        "siafi": "7107"
+      })
+
+      Bypass.expect(bypass, "GET", "#{cep}/json/", fn conn ->
+        conn
+        |> Conn.put_resp_header("content-type", "application/json")
+        |> Conn.resp(200, body)
+      end)
+
+      response = Client.get_cep_info(url, cep)
+
+      expected_response =
+        {:ok,
+         %{
+           "cep" => "01001-000",
+           "logradouro" => "Praça da Sé",
+           "complemento" => "lado ímpar",
+           "bairro" => "Sé",
+           "localidade" => "São Paulo",
+           "uf" => "SP",
+           "ibge" => "3550308",
+           "gia" => "1004",
+           "ddd" => "11",
+           "siafi" => "7107"
+         }}
+
+      assert expected_response == response
+    end
+
+    test "when cep is invalid, return an error", %{bypass: bypass} do
+      cep = "123"
+
+      url = endpoint_url(bypass.port)
+
+      Bypass.expect(bypass, "GET", "#{cep}/json/", fn conn ->
+        Conn.resp(conn, 400, "")
+      end)
+
+      response = Client.get_cep_info(url, cep)
+
+      expected_response = {:error, %Error{result: "Invalid cep", status: :bad_request}}
+
+      assert expected_response == response
+    end
+
+    test "when cep was not found, return a error", %{bypass: bypass} do
+      cep = "123"
+
+      body = ~s({"erro": true})
+
+      url = endpoint_url(bypass.port)
+
+      Bypass.expect(bypass, "GET", "#{cep}/json/", fn conn ->
+        conn
+        |> Conn.put_resp_header("content-type", "application/json")
+        |> Conn.resp(200, body)
+      end)
+
+      response = Client.get_cep_info(url, cep)
+
+      expected_response = {:error, %Error{result: "Cep not found", status: :not_found}}
+
+      assert expected_response == response
+    end
+
+    test "when there is a generic error, return a error", %{bypass: bypass} do
+      cep = "00000000"
+
+      url = endpoint_url(bypass.port)
+
+      Bypass.down(bypass)
+
+      response = Client.get_cep_info(url, cep)
+
+      expected_response = {:error, %Error{result: :econnrefused, status: :not_found}}
+
+      assert expected_response == response
+    end
+
+    defp endpoint_url(port), do: "http://localhost:#{port}/"
+  end
+end
