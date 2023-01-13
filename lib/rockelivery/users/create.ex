3b3f9d658995f87a7e950c32ef1e32dbb43dc5b7
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-01-13 18:26:20 -0300

    Feat - Call the right client

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -1,18 +1,21 @@
 defmodule Rockelivery.Users.Create do
   alias Rockelivery.{Error, Repo, User}
-  alias Rockelivery.ViaCep.Client
 
   def call(params) do
     cep = Map.get(params, "cep")
     user_changeset = User.changeset(params)
 
     with {:ok, %User{}} <- User.build(user_changeset),
-         {:ok, _cep_info} <- Client.get_cep_info(cep),
+         {:ok, _cep_info} <- client().get_cep_info(cep),
          {:ok, %User{}} = user <- Repo.insert(user_changeset) do
       user
     else
       {:error, %Error{}} = error -> error
       {:error, result} -> {:error, Error.build(:bad_request, result)}
     end
+  end
+
+  defp client do
+    Application.fetch_env!(:rockelivery, __MODULE__)[:via_cep_adapter]
   end
 end
