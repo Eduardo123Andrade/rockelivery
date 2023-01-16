a412610c10e472a86fe3452e7781be912977e6ab
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-01-16 13:17:35 -0300

    Feat - Created gen server for automatic generator report

---
+++ lib/rockelivery/orders/report_runner.ex
@@ -0,0 +1,33 @@
+defmodule Rockelivery.Orders.ReportRunner do
+  alias Rockelivery.Orders.Report
+  use GenServer
+
+  require Logger
+
+  # Client
+
+  def start_link(_initial_stack) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  @impl true
+  def init(state) do
+    schedule_report_generation()
+    {:ok, state}
+  end
+
+  @impl true
+  # recebe qualquer tipo de mensagem
+  # que nao tem handle_call ou handle_cast para receber a mensagem
+  def handle_info(:generate, state) do
+    Logger.info("Generate report...")
+
+    Report.create()
+    schedule_report_generation()
+    {:noreply, state}
+  end
+
+  defp schedule_report_generation do
+    Process.send_after(self(), :generate, 1000 * 20)
+  end
+end
